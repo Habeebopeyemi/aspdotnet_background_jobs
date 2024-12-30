e47cbfab6585287515f48c7373876c2957975a61
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2024-12-30 13:06:17 +0100

    feat: test cases for the background process implemented

---
+++ Tests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using API.Data;
+global using API.Services;
+global using FluentAssertions;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Time.Testing;
+global using Xunit;
---
+++ Tests/One-off/InitializationBackgroundServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using API.Services.Abstractions;
+using API.Services.One_off;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+
+namespace Tests.One_off
+{
+    public class InitializationBackgroundServiceTests
+    {
+        [Fact]
+        public async Task WhenExecuteAsyncIsInvoked_ThenSeedDatabaseAsyncMethodIsCalledOnce()
+        {
+            // Arrange
+            var worker = Substitute.For<IWorker>();
+            var serviceProvider = new ServiceCollection()
+                .AddDbContext<ApplicationDbContext>(options =>
+                    options.UseInMemoryDatabase("someDb"))
+                .BuildServiceProvider();
+            var sut = new InitializationHostedService(worker, serviceProvider);
+
+            // Act
+            await sut.StartAsync(CancellationToken.None);
+
+            // Assert
+            await worker.ReceivedWithAnyArgs(1)
+                .SeedDatabaseAsync(
+                    Arg.Any<ApplicationDbContext>(),
+                    Arg.Any<CancellationToken>());
+        }
+    }
+}
